9c3bd279a0dbd78f30cc8fa5de39255db12ad9aa
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-08-31 11:39:21 -0400

    creating layout using document.createElement

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-var quiz = document.querySelector("#start-btn");
+var body = document.body;
+
+var h1El = document.createElement("h1");
+var viewScore = document.createElement("p");
+var remainingTime = document.createElement("p");
+var questions = document.createElement("div");
+var pEl = document.createElement("p");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var inputEl = document.createElement("input");
+inputEl.setAttribute("type", "checkbox");
+
+// var btnEl = document.createElement("btn");
+
+// Store our li elements in a variable
+var listItems = document.getElementsByTagName("li");
+
+// Set the text content of relevant elements
+viewScore.textContent = "View Score";
+remainingTime.textContent = "Remaining time";
+h1El.textContent = "Coding Quiz Challenge";
+pEl.textContent =
+  "Try to answer the following code-related questions withen the time limit. Keep in mind that incorrect answer will penalize your scoretime by ten seconds!";
+
+// Append all of our elements
+
+body.append(viewScore);
+body.append(remainingTime);
+body.append(h1El);
+body.append(pEl);
+body.append(inputEl);
+
+// var quiz = document.querySelector("#start-btn");
 var currentQuestion = 0;
 var questions = [
   {
