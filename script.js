1413ebaea4147811c689751dd396a49027467d14
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-09-06 14:00:07 -0400

    displaying buttons properly

--- script.js
+++ script.js
@@ -1,88 +1,66 @@
-var body = document.body;
-
-var h1El = document.createElement("h1");
-var viewScore = document.createElement("p");
-viewScore.setAttribute("id", "p1");
-var remainingTime = document.createElement("p");
-remainingTime.setAttribute("id", "p2");
-var questions = document.createElement("div");
-questions.setAttribute("class", "container");
-var pEl = document.createElement("p");
-var listEl = document.createElement("ol");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-var inputEl = document.createElement("input");
-inputEl.setAttribute("type", "checkbox");
-
-// var btnEl = document.createElement("btn");
-
-// Store our li elements in a variable
-var listItems = document.getElementsByTagName("li");
-
-// Set the text content of relevant elements
-viewScore.textContent = "View Score";
-remainingTime.textContent = "Remaining time";
-h1El.textContent = "Coding Quiz Challenge";
-pEl.textContent =
-  "Try to answer the following code-related questions withen the time limit. Keep in mind that incorrect answer will penalize your scoretime by ten seconds!";
-
-// Append all of our elements
-
-body.append(viewScore);
-body.append(remainingTime);
-body.append(h1El);
-body.append(questions);
-questions.append(pEl);
-body.append(inputEl);
-
-// var quiz = document.querySelector("#start-btn");
+var quiz = document.getElementById("start-btn");
+var containerEl = document.querySelector(".main-container");
 var currentQuestion = 0;
 var questions = [
   {
-    q: "q1",
-    a: ["a1", "a2", "a3", "a4"],
-    c: "a1",
+    question: "abc",
+    answer: ["a", "b", "c", "d"],
+    correctAnswer: "b",
+    usesAnswer: "b",
+    responce: true,
+    time: 0,
   },
   {
-    q: "q2",
-    a: ["a1", "a2", "a3", "a4"],
-    c: "a4",
+    question: "abc",
+    answer: ["a", "b", "c", "d"],
+    correctAnswer: "b",
+    usesAnswer: "b",
+    responce: true,
+    time: 0,
   },
 ];
 
-var startingMinutes = 10;
-var time = startingMinutes * 60;
-var countdownEl = document.getElementById("countdown");
+function createRow(rows, text) {
+  for (var i = 0; i < rows; i++) {
+    var rowEl = document.createElement("div");
+    rowEl.setAttribute("class", "row");
+    rowEl.setAttribute("style", "display:flex");
+    var colEl = document.createElement("div");
+    colEl.setAttribute("class", "col");
+    colEl.setAttribute("style", "flex:50%");
 
-setInterval(updateCountdown, 1000);
-
-function startTimer() {
-  var minutes = Math.floor(time / 60);
-  var seconds = time % 60;
-  if (begin === "start") {
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
+    colEl.append(text);
+    rowEl.append(colEl);
+    containerEl.append(rowEl);
   }
 }
 
 function showQuestions() {
-  currentQuestion++;
-  if (currentQuestion === questions.length) {
-    endGame();
-  } else {
-    showQuestions();
+  containerEl.innerHTML = "";
+  var questionEl = document.createElement("h3");
+  questionEl.innerHTML = questions[currentQuestion].question;
+  createRow(1, questionEl);
+
+  var answerEl = "";
+
+  for (var i = 0; i < questions[currentQuestion].answer.length; i++) {
+    answerEl = document.createElement("button");
+    answerEl.setAttribute("class", "btn btn-secondary m-1");
+    answerEl.innerHTML = questions[currentQuestion].answer[i];
+    createRow(1, answerEl);
+
+    answerEl.addEventListener("click", function () {
+      questions[currentQuestion].usesAnswer =
+        questions[currentQuestion].answer[i];
+      answerCheck();
+      nextQuestion();
+    });
   }
 }
 
-function endGame() {
-  console.log("END");
-}
-
 quiz.addEventListener("click", function () {
-  console.log("start");
+  document.querySelector(".main-container").innerHTML = "";
+  // currentQuestion = 0;
   showQuestions();
-  startTimer();
+  //startTimer()
 });
