c5240558af69c9c9a5e206658c0801e9ef2773e8
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-09-06 20:15:50 -0400

    printning confirmatin on result

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ var containerEl = document.querySelector(".main-container");
 var currentQuestion = 0;
 var questions = [
   {
-    question: "abc",
-    answer: ["a", "b", "c", "d"],
-    correctAnswer: "b",
-    usesAnswer: "b",
-    responce: true,
+    question: "What is a variable?",
+    answer: [
+      "Store for your information",
+      "It let you group series of statement together to perform a special task",
+      "Variable is for dealing with more than one function at a time",
+      "Its used to remove elements from DOM",
+    ],
+    correctAnswer: "Store for your information",
+    usesAnswer: "Store for your information",
+    responce: false,
     time: 0,
   },
   {
-    question: "abc",
-    answer: ["a", "b", "c", "d"],
-    correctAnswer: "b",
-    usesAnswer: "b",
-    responce: true,
+    question: "Which one is 'AND LOGICAL' operator?",
+    answer: ["&&", "||", "!", "="],
+    correctAnswer: "&&",
+    usesAnswer: "&&",
+    responce: false,
     time: 0,
   },
 ];
@@ -41,21 +46,41 @@ function showQuestions() {
   questionEl.innerHTML = questions[currentQuestion].question;
   createRow(1, questionEl);
 
-  var answerEl = "";
-
-  for (var i = 0; i < questions[currentQuestion].answer.length; i++) {
+  questions[currentQuestion].answer.forEach((item) => {
     answerEl = document.createElement("button");
-    answerEl.setAttribute("class", "btn btn-secondary m-1");
-    answerEl.innerHTML = questions[currentQuestion].answer[i];
+    answerEl.setAttribute("class", "btn btn-primary m-2");
+    answerEl.innerHTML = item;
     createRow(1, answerEl);
 
     answerEl.addEventListener("click", function () {
       questions[currentQuestion].usesAnswer =
-        questions[currentQuestion].answer[i];
-      answerCheck();
-      nextQuestion();
+        questions[currentQuestion].answer[item];
+      correctAnswer();
+      // nextQuestion();
     });
+  });
+  function correctAnswer() {
+    if (
+      questions[currentQuestion].correctAnswer ===
+      questions[currentQuestion].usesAnswer
+    ) {
+      questions[currentQuestion].responce = false;
+      questions[currentQuestion].time = time;
+      document.getElementById("result").innerHTML = "correct";
+      setTimeout(function () {
+        document.getElementById("result").innerHTML = "";
+      }, 2000);
+    } else {
+      penaltyTime();
+      questions[currentQuestion].responce = true;
+      document.getElementById("result").innerHTML = "wrong";
+      setTimeout(function () {
+        document.getElementById("result").innerHTML = "";
+      }, 2000);
+    }
   }
+
+  function penaltyTime() {}
 }
 
 quiz.addEventListener("click", function () {
